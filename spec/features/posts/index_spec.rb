20caa0d9f12c897f407f57ddd9304ca973ad02be
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-08-31 08:08:49 +0300

    chore(postsPage): add visual feedback (#28)
    - add jumbotron that tell users there are no posts

--- spec/features/posts/index_spec.rb
+++ spec/features/posts/index_spec.rb
@@ -1,13 +1,34 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require_relative '../../support/login_form'
+require_relative '../../support/features/create_post_helper'
 
 feature 'view all posts' do
-  let!(:user) { create(:user) }
-  let!(:post_1) { create(:post, user: user, title: Faker::Dune.title) }
-  let!(:post_2) { create(:post, user: user) }
+  include CreatePostHelper
 
-  scenario 's(he) can view all posts' do
-    visit('/posts')
-    expect(page).to have_content(post_1.title)
-    expect(page).to have_content(post_2.title)
+  subject(:login_form) { LoginForm.new }
+  let(:user) { create(:user) }
+  let(:title) { Faker::Book.title }
+  let(:content) { Faker::Lovecraft.paragraph }
+
+  context 'when there are posts' do
+    scenario 's(he) can view all posts' do
+      login_form.visit_page.login_as(user)
+      create_post
+      expect(page).to have_content('Post created successfully')
+      visit(posts_path)
+
+      expect(page).to have_content(title)
+      expect(page).to have_content(content)
+    end
+  end
+
+  context 'when there are no posts' do
+    before { visit(posts_path) }
+
+    scenario 'users sees no posts available' do
+      expect(page).to have_content('Oops! There are no posts at the moment ;)')
+    end
   end
 end
