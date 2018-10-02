435babef85ccb3a67c613919c5683a485dc3b6e2
Gideon Kimutai <gideon.kimutai@andela.com>
2018-10-02 06:49:15 +0300

    chore(models): destroy related objects when any model is deleted (#44)

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,15 @@ require 'faker'
 user = User.create!(email: 'test_user@email.com', password: 'testpassword')
 
 100.times do |n|
-  Post.create(user: user, title: "#{Faker::Book.title} - #{n+=1}", content: "#{Faker::Lorem.paragraph * 10}")
+  post = Post.create(
+    user: user,
+    title: "#{Faker::Book.title} - #{n+=1}",
+    content: "#{Faker::Lorem.paragraph * 10}"
+  )
+end
+
+Post.all.each do |post|
+  5.times.each do
+    post.comments.create(comment: Faker::Lorem.paragraph, user: User.first)
+  end
 end
